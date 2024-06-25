d7cd2b0c2655bacdb10984729c4def9047861d8b
PICKQUE <925537712@qq.com>
2024-06-25 21:58:32 +0800

    add bitcoin format module

--- bitcoin/src/format.rs
+++ bitcoin/src/format.rs
@@ -1,8 +1,13 @@
 use core::fmt;
 use std::fmt::write;
 
-use gyu_model::format::Format;
+use gyu_model::{
+    address::AddressError, extended_private_key::ExtendedPrivateKeyError,
+    extended_public_key::ExtendedPublicKeyError, format::Format,
+};
 use serde::Serialize;
+
+use crate::network::BitcoinNetwork;
 
 #[derive(Serialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[allow(non_camel_case_types)]
@@ -15,7 +20,34 @@ pub enum BitcoinFormat {
 
 impl Format for BitcoinFormat {}
 
-impl BitcoinFormat {}
+impl BitcoinFormat {
+    pub fn to_address_prefix<N: BitcoinNetwork>(&self) -> Vec<u8> {
+        N::to_address_prefix(self)
+    }
+
+    pub fn from_address_prefix(prefix: &[u8]) -> Result<Self, AddressError> {
+        if prefix.len() < 2 {
+            return Err(AddressError::InvalidPrefix(prefix.to_vec()));
+        }
+
+        match (prefix[0], prefix[1]) {
+            (0x00, _) | (0x6F, _) => Ok(BitcoinFormat::P2PKH),
+            (0x05, _) | (0xC4, _) => Ok(BitcoinFormat::P2SH_P2WPKH),
+            (0x62, 0x63) | (0x74, 0x62) => Ok(BitcoinFormat::Bech32),
+            _ => return Err(AddressError::InvalidPrefix(prefix.to_vec())),
+        }
+    }
+
+    pub fn from_exteded_private_key_version_bytes(
+        prefix: &[u8],
+    ) -> Result<Self, ExtendedPrivateKeyError> {
+        match prefix[0..4] {
+            [0x04, 0x88, 0xAD, 0xE4] | [0x04, 0x35, 0x83, 0x94] => Ok(BitcoinFormat::P2PKH),
+            [0x04, 0x9D, 0x7C, 0xB2] | [0x04, 0x4A, 0x52, 0x62] => Ok(BitcoinFormat::P2SH_P2WPKH),
+            _ => Err(ExtendedPublicKeyError::InvalidVersionBytes(prefix.to_vec())),
+        }
+    }
+}
 
 impl fmt::Display for BitcoinFormat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
