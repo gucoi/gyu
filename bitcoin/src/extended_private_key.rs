d0ee22bfa22baf2dd104971dd20b83403cd2775b
PICKQUE <925537712@qq.com>
2024-07-25 21:40:38 +0800

    add display and fromstr for Extendedprivatekey

--- bitcoin/src/extended_private_key.rs
+++ bitcoin/src/extended_private_key.rs
@@ -2,16 +2,23 @@ use gyu_model::{
     derivation_path::ChildIndex,
     extended_private_key::{self, ExtendedPrivateKey, ExtendedPrivateKeyError},
     private_key::PrivateKey,
+    utilities::crypto::checksum,
 };
+
+use std::str::FromStr;
 
 use crate::{
     address::BitcoinAddress, format::BitcoinFormat, network::BitcoinNetwork,
     private_key::BitcoinPrivateKey, public_key::BitcoinPublicKey,
 };
 
+use base58::{FromBase58, ToBase58};
+
 use hmac::{Hmac, Mac};
 use secp256k1::{PublicKey, SecretKey};
 use sha2::Sha512;
+use std::fmt::{self, Display};
+
 type HmacSha512 = Hmac<Sha512>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -142,5 +149,75 @@ impl<N: BitcoinNetwork> ExtendedPrivateKey for BitcoinExtendedPrivateKey<N> {
         format: &Self::Format,
     ) -> Result<Self::Address, gyu_model::address::AddressError> {
         self.private_key.to_address(format)
+    }
+}
+
+impl<N: BitcoinNetwork> FromStr for BitcoinExtendedPrivateKey<N> {
+    type Err = ExtendedPrivateKeyError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let data = s.from_base58()?;
+        if data.len() != 82 {
+            return Err(ExtendedPrivateKeyError::InvalidByteLength(data.len()));
+        }
+
+        // Check that the version bytes correspond with the correct network.
+        let _ = N::from_extended_private_key_version_bytes(&data[0..4])?;
+        let format = BitcoinFormat::from_extended_private_key_version_bytes(&data[0..4])?;
+
+        let depth = data[4];
+
+        let mut parent_fingerprint = [0u8; 4];
+        parent_fingerprint.copy_from_slice(&data[5..9]);
+
+        let child_index = ChildIndex::from(u32::from_be_bytes(<[u8; 4]>::try_from(&data[9..13])?));
+
+        let mut chain_code = [0u8; 32];
+        chain_code.copy_from_slice(&data[13..45]);
+
+        let private_key = BitcoinPrivateKey::from_secp256k1_secret_key(
+            &SecretKey::parse_slice(&data[46..78])?,
+            true,
+        );
+
+        let expected = &data[78..82];
+        let checksum = &checksum(&data[0..78])[0..4];
+        if *expected != *checksum {
+            let expected = expected.to_base58();
+            let found = checksum.to_base58();
+            return Err(ExtendedPrivateKeyError::InvalidChecksum(expected, found));
+        }
+
+        Ok(Self {
+            format,
+            depth,
+            parent_fingerprint,
+            child_index,
+            chain_code,
+            private_key,
+        })
+    }
+}
+
+impl<N: BitcoinNetwork> Display for BitcoinExtendedPrivateKey<N> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let mut result = [0u8; 82];
+        result[0..4].copy_from_slice(
+            match &N::to_extended_private_key_version_bytes(&self.format) {
+                Ok(version) => version,
+                Err(_) => return Err(fmt::Error),
+            },
+        );
+        result[4] = self.depth;
+        result[5..9].copy_from_slice(&self.parent_fingerprint[..]);
+        result[9..13].copy_from_slice(&u32::from(self.child_index).to_be_bytes());
+        result[13..45].copy_from_slice(&self.chain_code[..]);
+        result[45] = 0;
+        result[46..78].copy_from_slice(&self.private_key.to_secp256k1_secret_key().serialize());
+
+        let checksum = &checksum(&result[0..78])[0..4];
+        result[78..82].copy_from_slice(&checksum);
+
+        fmt.write_str(&result.to_base58())
     }
 }
