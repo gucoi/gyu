8d27d044ddef483f12a7a8c23097eb4c6408b368
PICKQUE <925537712@qq.com>
2024-08-01 20:45:59 +0800

    add to phase

--- bitcoin/src/mnemonic.rs
+++ bitcoin/src/mnemonic.rs
@@ -91,7 +91,46 @@ impl<N: BitcoinNetwork, W: BitcoinWordlist> Mnemonic for BitcoinMnemonic<N, W> {
         }
     }
 
-    fn to_phrase(&self) -> Result<String, MnemonicError> {}
+    fn to_phrase(&self) -> Result<String, MnemonicError> {
+        let length: i32 = match self.entropy.len() {
+            16 => 12,
+            20 => 15,
+            24 => 18,
+            28 => 21,
+            32 => 24,
+            entropy_len => return Err(MnemonicError::InvalidEntropyLength(entropy_len)),
+        };
+
+        // Compute the checksum by taking the first ENT / 32 bits of the SHA256 hash
+        let mut sha256 = Sha256::new();
+        sha256.input(self.entropy.as_slice());
+
+        let hash = sha256.result();
+        let hash_0 = BitVec::<Msb0, u8>::from_element(hash[0]);
+        let (checksum, _) = hash_0.split_at(length.div(3) as usize);
+
+        // Convert the entropy bytes into bits and append the checksum
+        let mut encoding = BitVec::<Msb0, u8>::from_vec(self.entropy.clone());
+        encoding.append(&mut checksum.to_vec());
+
+        // Compute the phrase in 11 bit chunks which encode an index into the word list
+        let wordlist = W::get_all();
+        let phrase = encoding
+            .chunks(11)
+            .map(|index| {
+                // Convert a vector of 11 bits into a u11 number.
+                let index = index
+                    .iter()
+                    .enumerate()
+                    .map(|(i, &bit)| (bit as u16) * 2u16.pow(10 - i as u32))
+                    .sum::<u16>();
+
+                wordlist[index as usize]
+            })
+            .collect::<Vec<&str>>();
+
+        Ok(phrase.join(" "))
+    }
 
     fn to_private_key(&self, password: Option<&str>) -> Result<Self::PrivateKey, MnemonicError> {
         Ok(self.to_extended_private_key(password)?.to_private_key())
